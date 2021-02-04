c8f078e0c6b0400ab405234f8a9b12e292db1b04
Rui Teixeira <37545061+ruipfteixeira99@users.noreply.github.com>
2021-02-04 19:31:55 +0000

    Updated some files related to Vue.js

--- interface/main.js
+++ interface/main.js
@@ -4,26 +4,15 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            product: 'Socks',
-            image: './assets/images/parliament1.png',
-            url: 'https://www.parlamento.pt',
-            inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                {id: 2234, color:'green', image: './assets/images/parliament1.png'},
-                {id: 2235, color:'blue', image: './assets/images/parliament2.png'}
-            ]
+            premium: true
         }
     },
     methods: {
-        addToCart() {
+        updateCart() {
             this.cart += 1
         },
         removeFromCart() {
             this.cart -= 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
         }
     }
 })
