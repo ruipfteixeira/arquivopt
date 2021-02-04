717465692c07c5455c4e09b47270472f60a140a9
Rui Teixeira <37545061+ruipfteixeira99@users.noreply.github.com>
2021-02-04 02:04:00 +0000

    Added functions to work with XML.

---
+++ data/open_data/README.md
@@ -0,0 +1,17 @@
+# Leitura da Open Data do Parlamento
+
+## Iniciativas
+
+### Códigos de Leitura Importantes
+
+#### iniTipo
+
+Indica o tipo de iniciativa.
+
+A - Apreciação Parlamentar
+D - Projeto de Deliberação
+I - Inquérito Parlamentar
+J - Projeto de Lei
+P - Proposta de Lei
+R - Projeto de Resolução
+S - Proposta de Resolução
--- data/open_data/display.py
+++ data/open_data/display.py
@@ -3,6 +3,20 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('IniciativasXIII.xml')
 root = tree.getroot()
 
-for child in root:
-    for child1 in child.iter('iniTitulo'):
-        print(child1.text)
+# Return number of initiatives of a certain type
+def get_count(ini_type):
+    count = 0
+    for child in root.iter('iniTipo'):
+        if child.text == ini_type:
+            count = count + 1
+    return count
+
+# Returns statistical summary of initiatives type
+def get_stats():
+    return "Apreciação Parlamentar: " + str(get_count("A")) + "\n" + "Projeto de Deliberação: " + str(get_count("D")) + "\n" + "Inquérito Parlamentar: " + str(get_count("I")) + "\n" + "Projeto de Lei: " + str(get_count("J")) + "\n" + "Proposta de Lei: " + str(get_count("P")) + "\n" + "Projeto de Resolução: " + str(get_count("R")) + "\n" + "Proposta de Resolução: " + str(get_count("S"))
+
+# Returns initiative of a certain type
+def get_initiatives(ini_type):
+    for child in root.iter('iniTipo'):
+        if child.text == ini_type:
+            return child1.text
