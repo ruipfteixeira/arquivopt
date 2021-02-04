baf6692a5219b44f9b5cbf7ba6ab47f53a5d42a3
Rui Teixeira <37545061+ruipfteixeira99@users.noreply.github.com>
2021-02-04 15:57:54 +0000

    Continued to learn VUE.JS development.

--- interface/main.js
+++ interface/main.js
@@ -3,7 +3,27 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            cart: 0,
+            product: 'Socks',
+            image: './assets/images/parliament1.png',
+            url: 'https://www.parlamento.pt',
+            inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color:'green', image: './assets/images/parliament1.png'},
+                {id: 2235, color:'blue', image: './assets/images/parliament2.png'}
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            this.cart -= 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
